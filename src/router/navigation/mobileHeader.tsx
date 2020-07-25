4cc224b878a69fb980592c5a33b9403ed505be08
akat32 <baduk32127@gmail.com>
2020-07-25 17:40:10 +0900

    [ADD] NavLink Size & Link

--- src/router/navigation/mobileHeader.tsx
+++ src/router/navigation/mobileHeader.tsx
@@ -9,11 +9,10 @@ export const MobileHeader = props => {
             <div className="dump"/>
             <div className="openBtn" onClick = {()=> setIsOpen(isOpen => !isOpen)} />
             { isOpen ? <div className="headerBack" onClick = {() => setIsOpen(false) } /> : null}
-            <Menu lang = { props.lang } setLang = {props.setLang} isOpen = {isOpen}/>
+            <Menu lang = { props.lang } setLang = {props.setLang} setIsOpen = {()=> setIsOpen(false)}isOpen = {isOpen}/>
         </div>
     )
 }
-
 const Menu = props => {
     return (
         <div className="mobileHeaderMenu" style = {{ right: props.isOpen ? 0 : '-100vw'}}>
@@ -23,19 +22,19 @@ const Menu = props => {
                     <div className="nowLang">{ props.lang }</div>
                 </li>
                 <li className="innerLink">
-                    <NavLink exact to = '/' activeClassName = "activeMenu">Home</NavLink>
+                    <NavLink exact to = '/' activeClassName = "activeMenu" onClick = { ()=> props.setIsOpen() } >Home</NavLink>
                 </li>    
                 <li className="innerLink">
-                    <NavLink exact to = '/resume' activeClassName = "activeMenu">Resume</NavLink>
+                    <NavLink exact to = '/resume' activeClassName = "activeMenu" onClick = { ()=> props.setIsOpen() } >Resume</NavLink>
                 </li>    
                 <li className="innerLink">
-                    Github
+                    <a href = 'https://github.com/akat32' target="_blank" onClick = { ()=> props.setIsOpen() } >Github</a>
                 </li>    
                 <li className="innerLink">
-                    Blog
+                    <a href ='https://blog.naver.com/akat32' target="_blank" onClick = { ()=> props.setIsOpen() } >Blog</a>
                 </li>    
                 <li className="innerLink">
-                    <NavLink exact to = '/portfolio' activeClassName = "activeMenu">Portfolio</NavLink>
+                    <NavLink exact to = '/portfolio' activeClassName = "activeMenu" onClick = { ()=> props.setIsOpen() }>Portfolio</NavLink>
                 </li>
             </div>
         </div>
