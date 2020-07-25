ea9093af00b5e4c78d0e2c571940beccc20a5cbc
akat32 <baduk32127@gmail.com>
2020-07-26 04:24:46 +0900

    [ADD] Open Btn Image

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -1,47 +1,44 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.scss";
-import { HeaderNavigation } from "./navigation/header";
-import { MobileHeader } from './navigation/mobileHeader'
-import { Home, Portfolio, Resume } from "../component";
-import { DeviceProvider } from "../context/DeviceContext";
-import { ProjectProvider } from "../context/ProjectContext";
-import { isMobile } from "react-device-detect";
-import { EN } from "../TextPackage/EN.json";
-import { KR } from "../TextPackage/KR.json";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.scss';
+import { HeaderNavigation } from './navigation/header';
+import { MobileHeader } from './navigation/mobileHeader';
+import { Home, Portfolio, Resume } from '../component';
+import { DeviceProvider } from '../context/DeviceContext';
+import { ProjectProvider } from '../context/ProjectContext';
+import { isMobile } from 'react-device-detect';
+import { EN } from '../TextPackage/EN.json';
+import { KR } from '../TextPackage/KR.json';
 const App: React.FC = () => {
-  const [lang, setLang] = useState<string>("KR"); // KR, EN
-  return (
-    <Router>
-      <DeviceProvider>
-      <ProjectProvider>
-        {
-          isMobile ?
-          <>
-            <MobileHeader lang ={ lang } setLang = { setLang}/>
-          </> : 
-          <>
-            <HeaderNavigation lang={lang} setLang={setLang} />
-            <Switch>
-              <Route exact path="/">
-                <Home language={lang === "EN" ? EN.Home : KR.Home} lang={lang} />
-              </Route>
-              <Route exact path="/Portfolio">
-                <Portfolio
-                  language={lang === "EN" ? EN.Portfolio : KR.Portfolio}
-                  lang={lang}
-                />
-              </Route>
-              <Route exact path="/Resume">
-                <Resume lang={lang} />
-              </Route>
-            </Switch>
-          </>
-        }
-        </ProjectProvider>
-      </DeviceProvider>
-    </Router>
-  );
+	const [lang, setLang] = useState<string>('KR'); // KR, EN
+	return (
+		<Router>
+			<DeviceProvider>
+				<ProjectProvider>
+					{isMobile ? (
+						<>
+							<MobileHeader lang={lang} setLang={setLang} />
+						</>
+					) : (
+						<>
+							<HeaderNavigation lang={lang} setLang={setLang} />
+							<Switch>
+								<Route exact path="/">
+									<Home language={lang === 'EN' ? EN.Home : KR.Home} lang={lang} />
+								</Route>
+								<Route exact path="/Portfolio">
+									<Portfolio language={lang === 'EN' ? EN.Portfolio : KR.Portfolio} lang={lang} />
+								</Route>
+								<Route exact path="/Resume">
+									<Resume lang={lang} />
+								</Route>
+							</Switch>
+						</>
+					)}
+				</ProjectProvider>
+			</DeviceProvider>
+		</Router>
+	);
 };
 
 export default App;
--- src/router/navigation/mobileHeader.tsx
+++ src/router/navigation/mobileHeader.tsx
@@ -1,44 +1,57 @@
-import React, { useState } from 'react'
-import './mobile.scss'
-import { NavLink } from 'react-router-dom'
-export const MobileHeader = props => {
-    const [ isOpen, setIsOpen] = useState(false)
-    return (
-        <div className="mobileHeader">
-            <p className="userName">Park Tae Wook</p>
-            <div className="dump"/>
-            <div className="openBtn" onClick = {()=> setIsOpen(isOpen => !isOpen)} />
-            { isOpen ? <div className="headerBack" onClick = {() => setIsOpen(false) } /> : null}
-            <Menu lang = { props.lang } setLang = {props.setLang} setIsOpen = {()=> setIsOpen(false)}isOpen = {isOpen}/>
-        </div>
-    )
-}
-const Menu = props => {
-    return (
-        <div className="mobileHeaderMenu" style = {{ right: props.isOpen ? 0 : '-100vw'}}>
-            <div className="innerMenu">
-                <li className="langBox" onClick = { () => {
-                    props.setLang(lang => lang === "KR" ? "EN" : "KR")
-                }}>
-                    <div className="globalImg"/>
-                    <div className="nowLang">{ props.lang }</div>
-                </li>
-                <li className="innerLink">
-                    <NavLink exact to = '/' activeClassName = "activeMenu" onClick = { ()=> props.setIsOpen() } >Home</NavLink>
-                </li>    
-                <li className="innerLink">
-                    <NavLink exact to = '/resume' activeClassName = "activeMenu" onClick = { ()=> props.setIsOpen() } >Resume</NavLink>
-                </li>    
-                <li className="innerLink">
-                    <a href = 'https://github.com/akat32' target="_blank" onClick = { ()=> props.setIsOpen() } >Github</a>
-                </li>    
-                <li className="innerLink">
-                    <a href ='https://blog.naver.com/akat32' target="_blank" onClick = { ()=> props.setIsOpen() } >Blog</a>
-                </li>    
-                <li className="innerLink">
-                    <NavLink exact to = '/portfolio' activeClassName = "activeMenu" onClick = { ()=> props.setIsOpen() }>Portfolio</NavLink>
-                </li>
-            </div>
-        </div>
-    )
-}
+import React, { useState } from 'react';
+import './mobile.scss';
+import { NavLink } from 'react-router-dom';
+export const MobileHeader = (props) => {
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<div className="mobileHeader">
+			<p className="userName">Park Tae Wook</p>
+			<div className="dump" />
+			<div className="openBtn" onClick={() => setIsOpen((isOpen) => !isOpen)} />
+			{isOpen ? <div className="headerBack" onClick={() => setIsOpen(false)} /> : null}
+			<Menu lang={props.lang} setLang={props.setLang} setIsOpen={() => setIsOpen(false)} isOpen={isOpen} />
+		</div>
+	);
+};
+const Menu = (props) => {
+	return (
+		<div className="mobileHeaderMenu" style={{ right: props.isOpen ? 0 : '-100vw' }}>
+			<div className="innerMenu">
+				<li
+					className="langBox"
+					onClick={() => {
+						props.setLang((lang) => (lang === 'KR' ? 'EN' : 'KR'));
+					}}
+				>
+					<div className="globalImg" />
+					<div className="nowLang">{props.lang}</div>
+				</li>
+				<li className="innerLink">
+					<NavLink exact to="/" activeClassName="activeMenu" onClick={() => props.setIsOpen()}>
+						Home
+					</NavLink>
+				</li>
+				<li className="innerLink">
+					<NavLink exact to="/resume" activeClassName="activeMenu" onClick={() => props.setIsOpen()}>
+						Resume
+					</NavLink>
+				</li>
+				<li className="innerLink">
+					<a href="https://github.com/akat32" target="_blank" onClick={() => props.setIsOpen()}>
+						Github
+					</a>
+				</li>
+				<li className="innerLink">
+					<a href="https://blog.naver.com/akat32" target="_blank" onClick={() => props.setIsOpen()}>
+						Blog
+					</a>
+				</li>
+				<li className="innerLink">
+					<NavLink exact to="/portfolio" activeClassName="activeMenu" onClick={() => props.setIsOpen()}>
+						Portfolio
+					</NavLink>
+				</li>
+			</div>
+		</div>
+	);
+};
