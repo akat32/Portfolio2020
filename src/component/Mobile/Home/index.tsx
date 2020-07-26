4425980218b47c7b2be61797c87cda6456565f51
akat32 <baduk32127@gmail.com>
2020-07-26 22:43:17 +0900

    [CHANGE] EducationBox Style

--- src/component/Mobile/Home/index.tsx
+++ src/component/Mobile/Home/index.tsx
@@ -1,10 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 
+import { ResumeData } from '../../../context/ResumeData';
+import { ResumeDataEN } from '../../../context/ResumeDataEN';
 export const MobileHome = (props: any) => {
+	const [data, setData] = useState(props.lang === 'EN' ? ResumeDataEN : ResumeData);
 	return (
 		<div id="mobileHome">
 			<div className="backBlur" />
+			<p className="madeText">{props.language.madeText}</p>
+			<div className="resumeBox">
+				<div className="educationBox">
+					<p className="boxTitle">{props.language.education}</p>
+					{data.education.map((item, i) => {
+						return (
+							<div className="educationItem resumeItem" key={i}>
+								<div className="inner_box">
+									<div className="inner_info_box">
+										<img alt="커리어 아이콘" src={item.icon} className="carrerIcon" />
+										<div className="educationInfo info_info">
+											<div className="info_DesBox">
+												<p className="info_title">{item.school}</p>
+												<div className="dump" />
+												<p className="info_sub">{item.department}</p>
+											</div>
+											<div className="dump" />
+											<p className="info_date">
+												<span>{item.startAt}</span> ~ <span>{item.endAt !== '' ? item.endAt : '현재'}</span>
+											</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+				<div className="careerBox">
+					<p className="boxTitle">{props.language.career}</p>
+					{data.career.map((item, i) => {
+						return (
+							<div className="careerItem resumeItem" key={i}>
+								<div className="inner_box">
+									<div className="inner_info_box">
+										<img alt="커리어 아이콘" src={item.icon} className="carrerIcon" />
+										<div className="carrerInfo info_info">
+											<p className="info_title">{item.company}</p>
+											<p className="info_sub">{item.job}</p>
+											<p className="info_date">
+												<span>{item.startAt}</span> ~ <span>{item.endAt !== '' ? item.endAt : '현재'}</span>
+											</p>
+										</div>
+									</div>
+									<p className="info_des">{item.description}</p>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+				<div className="awardsBox">
+					<p className="boxTitle">{props.language.awards}</p>
+					{data.awards.map((item, i) => {
+						return (
+							<div className="awardsItem resumeItem" key={i}>
+								<div className="inner_box">
+									<div className="inner_info_box">
+										<div className="awardsInfo info_info">
+											<p className="info_title">{item.title}</p>
+											<p className="info_sub">{item.host}</p>
+											<p className="info_date">{item.date}</p>
+										</div>
+									</div>
+								</div>
+								<p className="info_des">{item.description}</p>
+							</div>
+						);
+					})}
+				</div>
+
+				<div className="certificateBox">
+					<p className="boxTitle">{props.language.certificate}</p>
+					{data.certificate.map((item, i) => {
+						return (
+							<div className="certificateItem resumeItem" key={i}>
+								<div className="inner_box">
+									<div className="inner_info_box">
+										<div className="awardsInfo info_info">
+											<p className="info_title">{item.title}</p>
+											<p className="info_sub">{item.host}</p>
+											<p className="info_date">{item.date}</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						);
+					})}
+				</div>
+			</div>
 		</div>
 	);
 };
